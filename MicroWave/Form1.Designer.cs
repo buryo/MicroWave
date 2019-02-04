74ee99f5476877b7d01f196581f26835e7b0510b
Burak Sen <s1127973@student.windesheim.nl>
2019-02-04 02:33:32 +0100

    first commit microwave application

--- MicroWave/Form1.Designer.cs
+++ MicroWave/Form1.Designer.cs
@@ -29,12 +29,227 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ButtonsPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.microwaveTimer = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.ButtonsPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // ButtonsPanel
+            // 
+            this.ButtonsPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonsPanel.ColumnCount = 2;
+            this.ButtonsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ButtonsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ButtonsPanel.Controls.Add(this.button1, 0, 0);
+            this.ButtonsPanel.Controls.Add(this.button2, 1, 0);
+            this.ButtonsPanel.Controls.Add(this.button3, 0, 1);
+            this.ButtonsPanel.Controls.Add(this.button4, 1, 1);
+            this.ButtonsPanel.Controls.Add(this.button5, 0, 2);
+            this.ButtonsPanel.Controls.Add(this.button6, 1, 2);
+            this.ButtonsPanel.Controls.Add(this.buttonOpen, 0, 3);
+            this.ButtonsPanel.Location = new System.Drawing.Point(572, 36);
+            this.ButtonsPanel.Name = "ButtonsPanel";
+            this.ButtonsPanel.RowCount = 4;
+            this.ButtonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.ButtonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.ButtonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.ButtonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.ButtonsPanel.Size = new System.Drawing.Size(200, 467);
+            this.ButtonsPanel.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 110);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pizza";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonPizza);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(103, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 110);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Fresh Vegetable";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttonVegetable);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(3, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 110);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Meat\r\n-----\r\nChicken";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.buttonMeat);
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(103, 119);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 110);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Potato";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.buttonPotato);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(3, 235);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 110);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Pause\r\n-----\r\nClear";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.ButtonPauseClear);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(103, 235);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(94, 110);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Start\r\n-----\r\n+30 second";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.ButtonStartAddSeconds);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Font = new System.Drawing.Font("Microsoft Tai Le", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOpen.Location = new System.Drawing.Point(3, 351);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(94, 113);
+            this.buttonOpen.TabIndex = 6;
+            this.buttonOpen.Text = "Open\r\n-----\r\nClose";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.ButtonDoor);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Image = global::MicroWave.Properties.Resources.microwave;
+            this.pictureBox1.Location = new System.Drawing.Point(19, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(527, 464);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // microwaveTimer
+            // 
+            this.microwaveTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.microwaveTimer.Location = new System.Drawing.Point(428, 155);
+            this.microwaveTimer.Name = "microwaveTimer";
+            this.microwaveTimer.Size = new System.Drawing.Size(61, 30);
+            this.microwaveTimer.TabIndex = 12;
+            this.microwaveTimer.Text = "00:00";
+            this.microwaveTimer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.microwaveTimer);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ButtonsPanel);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(800, 800);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Microwave";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ButtonsPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel ButtonsPanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TextBox microwaveTimer;
     }
 }
 
--- MicroWave/Form1.cs
+++ MicroWave/Form1.cs
@@ -12,9 +12,123 @@ namespace MicroWave
 {
     public partial class Form1 : Form
     {
+        protected bool MicrowaveStatus;
+        protected static bool Door;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// To exit the application through menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Timer with interval = 1000 (1 second)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (MicrowaveStatus)
+            {
+                MicrowaveTimer.Instance.CountDown();
+                microwaveTimer.Text = string.Format("{0}:{1}", MicrowaveTimer.Instance.Minute.ToString().PadLeft(2, '0'), MicrowaveTimer.Instance.Second.ToString().PadLeft(2, '0'));
+            }
+        }
+
+        private void buttonPizza(object sender, EventArgs e)
+        {
+            AddDishTimer(60);
+        }
+
+        private void buttonPotato(object sender, EventArgs e)
+        {
+            AddDishTimer(120);
+        }
+
+        private void buttonVegetable(object sender, EventArgs e)
+        {
+            AddDishTimer(300);
+        }
+
+        private void buttonMeat(object sender, EventArgs e)
+        {
+            AddDishTimer(800);
+        }
+
+        /// <summary>
+        /// Door control method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonDoor(object sender, EventArgs e)
+        {
+            switch (Door)
+            {
+                case false:
+                    Door = true;
+                    MicrowaveStatus = false;
+                    pictureBox1.Image = Properties.Resources.open_microwave;
+                    break;
+                case true:
+                    Door = false;
+                    MicrowaveStatus = true;
+                    pictureBox1.Image = Properties.Resources.microwave;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Starting microwave with fast 30 seconds
+        /// If microwave is paused, this method will continue the timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonStartAddSeconds(object sender, EventArgs e)
+        {
+            if (MicrowaveStatus || (MicrowaveTimer.Instance.Minute == 0 && MicrowaveTimer.Instance.Second == 0))
+            {
+                MicrowaveTimer.Instance.AddSeconds(30);
+            }
+            if (!Door)
+            {
+                MicrowaveStatus = true;
+            }
+        }
+
+        /// <summary>
+        /// Button to pause or if already paused, clearing method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonPauseClear(object sender, EventArgs e)
+        {
+            if (!MicrowaveStatus)
+            {
+                MicrowaveTimer.Instance.CounterReset();
+                microwaveTimer.Text = "00:00";
+            }
+            MicrowaveStatus = false;
+        }
+
+        /// <summary>
+        /// Time adder for dishes
+        /// </summary>
+        /// <param name="seconds"></param>
+        private void AddDishTimer(int seconds)
+        {
+            MicrowaveTimer.Instance.AddSeconds(seconds);
+            MicrowaveStatus = true;
         }
     }
 }
